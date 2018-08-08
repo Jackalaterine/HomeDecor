dd6e7ffb02ddb94085e4f08e7b322b220917b74a
carolineport <cport@ymail.com>
2018-08-08 15:14:47 -0500

    Initial commit

--- HomeDecor/ViewController.swift
+++ HomeDecor/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+     let config = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        config.planeDetection = .horizontal
+        sceneView.session.run(config)
+        
+        sceneView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createFloorNode(anchor:ARPlaneAnchor) ->SCNNode{
+        let floorNode = SCNNode(geometry: SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))) //1
+        
+        floorNode.position=SCNVector3(anchor.center.x,0,anchor.center.z)                                               //2
+        floorNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "wood")                                            //3
+        floorNode.geometry?.firstMaterial?.isDoubleSided = true                                                        //4
+        floorNode.eulerAngles = SCNVector3(Double.pi/2,0,0)                                                    //5
+        return floorNode                                                                                               //6
     }
+}
 
-
+    extension ViewController:ARSCNViewDelegate{
+        func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+            guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+            let planeNode = createFloorNode(anchor: planeAnchor)
+            node.addChildNode(planeNode)
+        }
+        
+        func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+            guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+            node.enumerateChildNodes { (node, _) in
+                node.removeFromParentNode()
+            }
+            let planeNode = createFloorNode(anchor: planeAnchor)
+            node.addChildNode(planeNode)
+        }
+        
+        func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+            guard let _ = anchor as? ARPlaneAnchor else {return}
+            node.enumerateChildNodes { (node, _) in
+                node.removeFromParentNode()
+            }
+        }
 }
 
